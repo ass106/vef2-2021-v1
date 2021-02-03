fe526167dd8af0dc2614e6d0f3b710a888b073f2
ass106 <ass@hi.is>
2021-02-03 21:44:26 +0000

    Error handling

--- app.js
+++ app.js
@@ -1,3 +1,28 @@
+/**
+ * Middleware for 404 not found
+ * @param {object} req
+ * @param {object} res
+ * @param {function} next
+ */
+function notFoundHandler(req, res, next) { // eslint-disable-line
+  const title = 'Síða fannst ekki';
+  const message = 'Þetta er ekki síðan sem þú leitar að...';
+  res.status(404).render('error', { title, message });
+}
+
+/**
+ * Middleware for error handling.
+ * @param {object} err
+ * @param {object} req
+ * @param {object} res
+ * @param {function} next
+ */
+function errorHandler(err, req, res, next) { // eslint-disable-line
+  const title = 'Villa kom upp';
+  const message = 'Það kom upp villa, villtu vinsamlegast hætta þessu..';
+  res.status(500).render('error', { title, message });
+}
+
 const path = require('path');
 const express = require('express');
 const videos = require('./src/videos');
@@ -16,6 +41,9 @@ app.use('/', videos);
 
 app.set('views', viewsPath);
 app.set('view engine', 'ejs');
+
+app.use(notFoundHandler);
+app.use(errorHandler);
 
 app.listen(port, host, () => {
   console.log(
